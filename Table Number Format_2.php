eeb98dcc69c695f991d09500219dc5cb07dcc0fc
Yash Gupta <90241440+yash8917@users.noreply.github.com>
2022-09-07 21:22:15 +0530

    Initial Commit

---
+++ Table Number Format_2.php
@@ -0,0 +1,13 @@
+<?php
+//$num=0;
+echo "<table style=' border-collapse: collapse'>";
+for ($i=0;$i<10;$i++){
+    echo "<tr>";
+    for($k=0; $k <=9 ; $k++){
+        echo " <td style='border: 3px dashed aqua'><b>".$i.""."$k</b></td>";
+    }echo '</tr>';
+}echo '</table>'
+
+
+
+?>
