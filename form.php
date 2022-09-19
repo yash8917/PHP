f28631dbc48ee4f73785756457742a9e81e78ce0
Yash Gupta <90241440+yash8917@users.noreply.github.com>
2022-09-19 21:48:45 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+    <input type="submit">
+        <table>
+            <tr>
+                <td><label for="">Name</label></td>
+                <!-- input cocntrol -->
+                <td> <input type="text" value="yash" readonly  name="userName" placeholder="Enter Your name"  autocomplete="off" autofocus="off" ></td></tr>
+            <tr>
+                <td><label for="">Contact Number</label></td>
+                <td><input type="number"   name="userName" placeholder="Enter Your number"  autocomplete="off"></td>  
+                <!-- password , email , add. -->
+            </tr>
+            <tr>
+                <td><label for="">Gender</label></td>
+                <td><input type="radio" name="Gender">M</td>
+                <td><input type="radio" name="Gender">F</td>
+            </tr>
+            <tr>
+                <td><label for="">Date</label></td>
+               <td><input type="date"></td>
+            </tr>
+           
+        </table>
+    </form>
+</body
+</html>
