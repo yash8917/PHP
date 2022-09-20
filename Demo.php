cfcdc0eed8a21eee71d644edd69aff536191ca47
Yash Gupta <90241440+yash8917@users.noreply.github.com>
2022-09-20 19:28:02 +0530

    Initial Commit

---
+++ Demo.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="">
+    <table>
+        <tr>
+            <label for="">Name</label>
+            <!-- input cocntrol -->
+            <td> <input type="text" value="yash" readonly  name="userName" placeholder="Enter Your name"  autocomplete="off" autofocus="off" ></td></tr>
+        <tr>
+            <td><label for="">Contact Number</label></td>
+            <td><input type="password"   name="userName" placeholder="Enter Your number"  autocomplete="off"></td>
+            <!-- password , email -->
+        </tr>
+    </table>
+</form>
+</body>
+</html>
