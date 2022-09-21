b7490c4fe195ac82bed52aa2d4031c001a552182
Yash Gupta <90241440+yash8917@users.noreply.github.com>
2022-09-21 07:31:00 +0530

    Initial Commit

---
+++ Module_2/data.php
@@ -0,0 +1,5 @@
+<?php
+$val_one =$_POST["num1"];
+$val_second =$_POST["num2"];
+echo $val_one+$val_second;
+?>
---
+++ Module_2/form - navigate to php.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="data.php" method="post">
+    
+    Enter the first number <input type="number" name="num1"><br>
+    Enter the second number<input type="number" name="num2">
+    <input type="submit" value="Addition">
+  
+     </form>
+</body>
+</html>
+
