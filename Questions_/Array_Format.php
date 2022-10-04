92ff7818dc2e05586ee8ad49a38205f41b46da9f
Yash Gupta <90241440+yash8917@users.noreply.github.com>
2022-10-04 19:36:59 +0530

    Initial Commit

--- Questions_/Array_Format.php
+++ Questions_/Array_Format.php
@@ -1,18 +1,8 @@
 <?php
 $str ="Our teacher is brilliant";
-//echo var_dump((array)ord($str));
-//print_r($str);
-//$str_ar=(array)$str;
-
-foreach (count_chars($str,1) as $i=>$val){
-//    echo "there $val of " ,ord($i),"<br>";
-//    var_dump((array)$val,ord($i));
-    $ar=["$i"=>$val];
-//   echo var_dump($ar);
-print_r($ar);
+print_r(count_chars($str,1));
 
 
 
 
-}
 ?>
