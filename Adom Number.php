857b58e3241a9566c6b36bbc15286d8c7045da53
Yash Gupta <90241440+yash8917@users.noreply.github.com>
2022-10-13 20:56:48 +0530

    Initial Commit

--- Adom Number.php
+++ Adom Number.php
@@ -1,1 +1,28 @@
 <!--write a php script to showthe working of  calculator having addi , sub , divi, modulo ,mul-->
+
+<?php
+
+function reverse($num){
+    $res=0;
+    while($num >1){
+        $res = $res  * 10 +$num % 10;
+        $num=(int)$num/10;
+    }
+    return $res;
+}
+function square($num){
+    return ($num**2);
+}
+function  adamNo($num)
+{
+    $a = square($num);
+    $b = square(reverse($num));
+
+    if ($a == reverse($b)){
+       return true;
+    }else{
+        return false;
+    }
+}
+$r=adamNo(14);
+echo $r;
---
+++ Questions_/Arrange the Correct order.php
@@ -0,0 +1,4 @@
+<?php
+$val="aBbc011";
+print_r(count_chars($val,3));
+echo "<br>";
