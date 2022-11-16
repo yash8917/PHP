b01a14d0a26e53d6e73ff12a8edbd57ab4b04f1d
Yash Gupta <90241440+yash8917@users.noreply.github.com>
2022-11-17 01:20:09 +0530

    Initial Commit

---
+++ Module_2/Cookie's/Set_and_Get_Cookies.php
@@ -0,0 +1,18 @@
+<?php
+$cookie_name =$_COOKIE['user'];
+$cookie_value = $_COOKIE['Anvesh'];
+// set the value
+setcookie($cookie_name , $cookie_value , time()+ 3600 ,"/","amazon.com", false);
+?>
+<html>
+<body>
+<?php
+if(!$_COOKIE['user']){
+    echo " Cookie is not set";
+}else {
+    echo $_COOKIE[$cookie_value];
+}
+?>
+
+</body>
+</html>
